a4e46178d7c2d6167699b3c088fa8739a089fda9
dejinassar <dejinassar@gmail.com>
2025-10-31 21:41:37 +0100

    fix: corrected ports and nginx upstream configuration

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-# Generate nginx.conf from template
-envsubst '$ACTIVE_POOL $INACTIVE_POOL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+set -e
 
-# Start nginx in the foreground
+echo "Starting nginx with pool: ${ACTIVE_POOL}"
+
+# Replace env vars in template
+envsubst '$ACTIVE_POOL $ACTIVE_RELEASE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+echo "Generated nginx.conf:"
+cat /etc/nginx/nginx.conf | grep proxy_set_header
+
 nginx -g 'daemon off;'
